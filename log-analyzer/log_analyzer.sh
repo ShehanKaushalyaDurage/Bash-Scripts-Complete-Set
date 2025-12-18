7df03d26c9a284897a8b4fd1da28786d193684c2
shehan kaushalya <147157691+ShehanKaushalyaDurage@users.noreply.github.com>
2025-12-18 13:58:39 +0530

    Add log analyzer script with alert system
    This script analyzes a log file for errors, warnings, and info messages, and alerts if the error threshold is exceeded.

--- log-analyzer/log_analyzer.sh
+++ log-analyzer/log_analyzer.sh
@@ -1,1 +1,33 @@
+#!/bin/bash
+#auther : Shehan Kaushalya
+#description: Log Analyzer + Alert System
+#date:18/12/2025
+
+LOG_FILE=$1
+ERROR_THRESHOLD=5
+
+if [[ ! -f $LOG_FILE ]]; then
+  echo "Log file not found"
+  exit 1
+fi
+
+TOTAL=$(wc -l < "$LOG_FILE")
+ERRORS=$(grep -i "error" "$LOG_FILE" | wc -l)
+WARNINGS=$(grep -i "warn" "$LOG_FILE" | wc -l)
+INFOS=$(grep -i "info" "$LOG_FILE" | wc -l)
+
+TOP_ERROR=$(grep -i "error" "$LOG_FILE" | awk '{$1=""; print}' | sort | uniq -c | sort -nr | head -1)
+
+echo "Total lines: $TOTAL"
+echo "Errors: $ERRORS"
+echo "Warnings: $WARNINGS"
+echo "Info: $INFOS"
+echo "Top Error: $TOP_ERROR"
+
+if (( ERRORS > ERROR_THRESHOLD )); then
+  echo "ALERT: Error threshold exceeded"
+  exit 2
+fi
+
+exit 0
 
